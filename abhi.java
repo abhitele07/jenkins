04bf02334cbb98820679573e5ccfbe8860de1a9a
root <root@ip-172-31-7-200.ap-south-1.compute.internal>
2023-07-13 13:06:53 +0000

    mistake wala

--- abhi.java
+++ abhi.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Certainly! Here's a simple Java code example for practicing:");
+        System.out.println(" It is Certainly! Here's a simple Java code example for practicing:");
     }
 }
